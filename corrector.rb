cc3b4a5e6b7ba828fa6c9c26f0f3590ece137e3b
mike2611 <mpuentes2611@gmail.com>
2021-12-14 21:25:03 -0600

    Create classcorrector

---
+++ corrector.rb
@@ -0,0 +1,6 @@
+class Corrector
+  def corrector_name(name)
+    name_array = name.split("")
+    name = name_array[0].upcase + name_array[1, name.length].join("")
+    name.length > 10 ? name.slice(0, 9) : name
+  end
