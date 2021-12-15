ec36031add112fa486bb691dff37dae772817308
mike2611 <mpuentes2611@gmail.com>
2021-12-14 22:32:17 -0600

    Add validate_name method to Person class

--- person.rb
+++ person.rb
@@ -1,9 +1,12 @@
+require './corrector'
+
 class Person
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @correct_name = Corrector.new
   end
   attr_reader :id, :parent_permission
   attr_accessor :name, :age
@@ -18,5 +21,9 @@ class Person
     return true if person.of_age?(person) || person.parent_permission
 
     false
+  end
+
+  def validate_name()
+    @name = @correct_name.corrector_name(@name)
   end
 end
