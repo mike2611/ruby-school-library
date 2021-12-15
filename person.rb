5dc6b439f341f510d724b21ab3f013877afdc729
mike2611 <mpuentes2611@gmail.com>
2021-12-14 22:29:14 -0600

    Undo changes

--- person.rb
+++ person.rb
@@ -4,6 +4,7 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @correct_name = Corrector.new
   end
   attr_reader :id, :parent_permission
   attr_accessor :name, :age
