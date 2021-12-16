d0ea7f53bb7533d200b7f8b26b83f06b7128a43a
mike2611 <mpuentes2611@gmail.com>
2021-12-15 20:47:07 -0600

    Fix linter errors

--- person.rb
+++ person.rb
@@ -2,10 +2,9 @@ require './corrector'
 require 'pry'
 
 class Person
-
   attr_reader :id, :parent_permission, rentals
   attr_accessor :name, :age
-  
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
@@ -17,11 +16,13 @@ class Person
 
   def of_age?(person)
     return true if person.age >= 18
+
     false
   end
 
   def can_use_services?(person)
     return true if person.of_age?(person) || person.parent_permission
+
     false
   end
 
--- rental.rb
+++ rental.rb
@@ -1,6 +1,7 @@
 class Rental
   attr_reader :date
   attr_accessor :book, :person
+
   def initialize(date, book, person)
     @date = date
     @book = book
